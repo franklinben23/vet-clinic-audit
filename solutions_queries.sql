c54a8483222687e5e3c7c4b96431f6ffa256e650
franklin <franklinbenjamin032@gmail.com>
2022-05-04 12:40:30 -0400

    add first solution

--- solutions_queries.sql
+++ solutions_queries.sql
@@ -1,7 +1,9 @@
 # First Query
-SELECT COUNT(*) FROM visits where animal_id = 4;
+SELECT COUNT(*)
+FROM visits
+where animal_id = 4;
 
 # Solution for first query
-CREATE INDEX animal_id_on_visits ON visits (animal_id ASC)
+CREATE INDEX animals_visitis_id ON visits(animal_id ASC);
 
 
